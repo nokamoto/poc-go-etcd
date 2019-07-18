57f8461acf86b4fa95500dab59c44c94426c352e
Naofumi Okamoto <nokamoto.engr@gmail.com>
2019-07-18 13:53:02 +0900

    Run Get by prefix (#5)

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/etcd-io/etcd/clientv3"
 	"log"
 	"time"
@@ -12,19 +13,8 @@ var (
 	endpoint = flag.String("endpoint", "etcd:2379", "etcd endpoints")
 )
 
-func main() {
-	flag.Parse()
-
-	log.Println("hello")
-
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{*endpoint},
-		DialTimeout: 2 * time.Second,
-	})
-	if err != nil {
-		panic(err)
-	}
-
+func AccessEtcd(cli *clientv3.Client) {
+	// https://etcd.io/docs/v3.3.12/demo/#access-etcd
 	ctx := context.Background()
 
 	res, err := cli.Put(ctx, "foo", "bar")
@@ -40,4 +30,47 @@ func main() {
 	}
 
 	log.Printf("get(foo): %v", got)
+}
+
+func GetByPrefix(cli *clientv3.Client) {
+	// https://etcd.io/docs/v3.3.12/demo/#get-by-prefix
+
+	ctx := context.Background()
+	prefix := "foo"
+
+	for i := 0; i < 10; i++ {
+		k := fmt.Sprintf("%s%d", prefix, i)
+		v := fmt.Sprintf("bar%d", i)
+
+		res, err := cli.Put(ctx, k, v)
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("put(%s, %s): %v", k, v, res)
+	}
+
+	res, err := cli.Get(ctx, prefix, clientv3.WithPrefix())
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("get(%s) with prefix: %v", prefix, res)
+}
+
+func main() {
+	flag.Parse()
+
+	log.Println("hello")
+
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{*endpoint},
+		DialTimeout: 2 * time.Second,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	AccessEtcd(cli)
+	GetByPrefix(cli)
 }
